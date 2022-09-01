6d79484c3ea5471e0ef3ebb81626c8e34e56bf88
abe520 <abe520@msn.com>
2022-09-01 22:36:54 +0800

    2

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,5 +1,5 @@
 # Modify Default IP
-sed -i 's/192.168.1.1/192.168.12.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.12.13/g' package/base-files/files/bin/config_generate
 #
 # 修改连接数
 #sed -i 's/net.netfilter.nf_conntrack_max=.*/net.netfilter.nf_conntrack_max=65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
