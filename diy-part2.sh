e81e610747087e1f3fdd45ad514aae9d32b759c2
abe520 <abe520@msn.com>
2022-09-02 16:50:51 +0800

    2

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,7 +17,7 @@ git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netd
 #
 #添加额外非必须软件包
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+#git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 #git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 #git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
 git clone https://github.com/riverscn/openwrt-iptvhelper.git package/openwrt-iptvhelper
